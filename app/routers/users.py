68b9491344ceeb7f05f14d89aa216b226b517fe9
AndreaAzzu <a.azzu2@studenti.unica.it>
2025-05-29 10:26:34 +0200

    implementato GET /users/{username} (DA TESTARE) e implementata DELETE/users/{username} (TESTATA e FUNZIONANTE

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from sqlmodel import select, delete
 from app.data.db import SessionDep
 from app.models.user import User, UserCreate, UserPublic
+from typing import Annotated
+
 
 router = APIRouter(prefix="/users")
 
@@ -36,3 +38,27 @@ def delete_all_users(session: SessionDep):
     session.exec(statement)
     session.commit()
     return "All users successfully deleted"
+
+@router.get("/users/{username}", response_model=UserPublic)
+def get_user_by_username(
+    session: SessionDep,
+    username: Annotated[str, Path(description="The username of the user to get")]
+) -> UserPublic:
+    """Returns the user with the given username."""
+    user = session.get(User, username)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.delete("/{username}")
+def delete_user(
+    session: SessionDep,
+    username : Annotated[str, Path(description="The username of the user to delete")]
+):
+    """Deletes the user with the given ID."""
+    user = session.get(User, username)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return "User successfully deleted"
